cb8edb377b4f55f2b225d937eecf822fca6d34c8
bokket <3100563328@qq.com>
2021-05-27 00:55:19 +0800

    commit

--- bokket/fd_manager/hook.h
+++ bokket/fd_manager/hook.h
@@ -10,6 +10,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <stdint.h>
 
 
 namespace bokket
@@ -40,9 +41,54 @@ extern nanosleep_ptr g_sys_nanosleep;
 typedef int (*socket_ptr)(int domain,int type,int protocol);
 extern socket_ptr g_sys_socket;
 
+typedef int (*connect_ptr)(int sockfd,const struct sockaddr* addr,socklen_t addrlen);
+extern connect_ptr g_sys_connect;
+
+typedef int (*accept_ptr)(int sockfd,struct sockaddr* addr,socklen_t* addrlen);
+extern accept_ptr g_sys_accept;
 
 
+typedef ssize_t (*read_ptr)(int fd,void* buf,size_t count);
+extern read_ptr g_sys_read;
 
+typedef ssize_t (*readv_ptr)(int fd,const struct iovec* iov,int iovcnt);
+extern readv_ptr g_sys_readv;
+
+typedef ssize_t (*recv_ptr)(int sockfd,void* buf,size_t len,int flags);
+extern recv_ptr g_sys_recv;
+
+typedef ssize_t (*recvfrom_ptr)(int sockfd,void* buf,size_t len,int flags,struct sockaddr* src_addr,socklen_t* addrlen);
+extern recvfrom_ptr g_sys_recvfrom;
+
+typedef ssize_t (*recvmsg_ptr)(int sockfd,struct msghdr* msg,int flags);
+extern recvfrom_ptr g_sys_recvmsg;
+
+typedef ssize_t (*write_ptr)(int fd,const void* buf,size_t count);
+extern write_ptr g_sys_write;
+
+typedef ssize_t (*writev_ptr)(int fd,const struct iovec* iov,int iovcnt);
+extern writev_ptr g_sys_write;
+
+typedef ssize_t (*send_ptr)(int s,const void* msg,size_t len,int flags);
+extern send_ptr g_sys_send;
+
+typedef ssize_t (*sendto_ptr)(int s,const void* msg,size_t len,int flags,const struct sockaddr* to,socklen_t tolen);
+extern sendto_ptr g_sys_sendto;
+
+typedef ssize_t (*sendmsg_ptr)(int s, const struct msghdr *msg, int flags);
+extern sendmsg_ptr g_sys_sendmsg;
+
+typedef int (*close_ptr)(int fd);
+extern close_ptr g_sys_close;
+
+typedef int (*fcntl_ptr)(int fd, int cmd, ...);
+extern fcntl_ptr g_sys_fcntl;
+
+typedef int (*setsockopt_ptr)(int sockfd, int level, int optname, const void *optval, socklen_t optlen);
+extern setsockopt_ptr g_sys_setsockopt;
+
+// 自定义超时连接函数
+extern int connect_with_timeout(int fd, const struct sockaddr* addr, socklen_t addrlen, int64_t timeout_ms);
 
 }
 
